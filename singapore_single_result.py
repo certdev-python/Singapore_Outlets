f0eb221f72dfb05b6874da3f0a9845f3a23136c3
certdev-python <121555761+certdev-python@users.noreply.github.com>
2023-02-14 12:52:40 +0530

    Update singapore_single_result.py

--- singapore_single_result.py
+++ singapore_single_result.py
@@ -19,52 +19,157 @@ driver = uc.Chrome(chrome_options=opts)
 # # Try accessing a website with antibot service 
 driver.get("https://www.sgpbusiness.com/")
 time.sleep(5)
-urls,name = [],[]
 
-for i in business_name:
+# find out the results 
+data= []
+
+for i in new_name:
     
+    print("For this url",i)  
     # search business_Name
     search = driver.find_elements(By.ID, "nav_search_val")
-    search[0].send_keys(i)
-
-    # click button 
+    try:
+        search[0].send_keys(i)
+    except:
+        time.sleep(8)
+    # click button
     search_button = driver.find_elements(By.XPATH, "/html/body/nav/div/div/form/div/div[2]/span/button")
-    search_button[0].click()
+    try:
+        search_button[0].click()
+    except:
+        time.sleep(8)
+        
+    try:
+        list_ = driver.find_elements(By.XPATH, '/html/body/div[1]/div/div[3]/div/div')
+        time.sleep(1)
+        result = list_[0].text.split('\n')
+        print(result)
+        print(len(result))
+        # for no result 
+    except:
+        print("except")
+        store=["error found on page" for i in range(5)]
+        store.append(i)
+        data.append(store)
+        
+    store=[]
+    # for no result
+    if len(result)==1:
+        print("No")
+        store=["Not Found" for i in range(4)]
+        store.append(" ")
+        store.append(i)
+        data.append(store)
 
-    # # redirect to the next page where we will find the business is live 
-    # # scroll down until the element is found
 
-    element = driver.find_elements(By.XPATH, "/html/body/div[1]/div/div[3]/div/div/a/div/div/h6")
-    driver.execute_script("arguments[0].scrollIntoView();", element[0])
-    time.sleep(1)
-    
-    # check condition for single element
-    if len(element) <= 1:
+    # for single result found
+    elif len(result)==2: 
+        time.sleep(1)
+        href = driver.find_elements(By.XPATH, '//div[@class="list-group list-group-flush"]/a')
 
-        # check if company is alive 
-        if 'Live Company' in driver.find_elements(By.CLASS_NAME, "media-body")[0].text.split('\n')[1]:
+        c = 0
+        l = []
+        for d in href:
+            dict_details = {}
+            dict_details['name'] = result[c]
+            dict_details['status'] = result[c + 1].split("U")[0]
+            dict_details['registered']='Registered' in result[c + 1].split("U")[0]
+            dict_details['url'] = d.get_attribute('href')
+            l.append(dict_details)
+            c += 2
 
-            # # click on the element
-            element[0].click()
+        for j in l:
+            store = []
+            if (j['registered'] or j['status'].startswith('Live')):
+                driver.get(j['url'])
+#                 print(j['url'])
+                time.sleep(8)
+                try:
+                    link = driver.find_element(By.XPATH, "//*[@id='Contact-Information']/div/ul/li/span/small/a")
+                    store.append(link.get_attribute('href'))        
+                except:
+                    try:
+                        link=driver.find_element(By.XPATH, "//*[@id='Contact-Information']/div/ul/li[2]/ul/li[1]/span/small/a")
+                        store.append(link.get_attribute('href'))        
+                    except:
+                        store.append('NA')
 
-            # close the google Vignette
-            driver.refresh()
+                try:
+                    # Address
+                    store.append(driver.find_elements(By.XPATH, "//span[contains(@itemprop, 'address')]")[0].text)
+                except:
+                    store.append('NA')
+                try:
+                    # Buiness_name
+                    store.append(driver.find_element(By.XPATH,'//*[@id="Overview"]/div[1]/h1').text)
+                except:
+                    store.append('NA')
+                store.append("Found")
+                store.append('1')
+                store.append(i)
+                data.append(store)
+            else:
+                time.sleep(1)
+                store=["Not Found" for i in range(2)]
+                store.append(i)
+                store.append("Found")
+                store.append('0')
+                store.append(i)
+                data.append(store)
+#               
 
-            element = driver.find_elements(By.XPATH, "/html/body/div[1]/div/div[3]/div/div/a/div/div/h6")
-            driver.execute_script("arguments[0].scrollIntoView();", element[0])
+    else:
+         
+        time.sleep(1)
+        # for multiple result found
+        href = driver.find_elements(By.XPATH, '//div[@class="list-group list-group-flush"]/a')
+        time.sleep(1)
+        f=0          
+        c = 0
+        l = []
+        for d in href:
+            dict_details = {}
+            dict_details['name'] = result[c]
+            dict_details['status'] = result[c + 1].split("U")[0]
+            dict_details['registered']='Registered' in result[c + 1].split("U")[0]
+            dict_details['url'] = d.get_attribute('href')
+            l.append(dict_details)
             time.sleep(1)
-            # # click on the element
-            element[0].click()
+            c += 2
+        for j in l:
+            store = []
+            if (j['registered'] or j['status'].startswith('Live')) and ('FOOD' in j['name'] or 'BAR' in j['name'] or 'RESTAURANTS' in j['name'] or 'DISCO' in j['name'] or 'LIQUOR' in j['name'] or 'CLUB' in j['name'] or 'WINE' in j['name']):
+                driver.get(j['url'])
+                time.sleep(8)
 
-            # get google map url 
-            link = driver.find_elements(By.XPATH, "//div[@class='card-body pb-3']//a[@href]")
+                try:
+                    link = driver.find_element(By.XPATH, "//*[@id='Contact-Information']/div/ul/li/span/small/a")
+                    store.append(link.get_attribute('href'))        
+                except:
+                    try:
+                        link=driver.find_element(By.XPATH, "//*[@id='Contact-Information']/div/ul/li[2]/ul/li[1]/span/small/a")
+                        store.append(link.get_attribute('href'))        
+                    except:
+                        store.append('NA')
+                try:
+                    store.append(driver.find_elements(By.XPATH, "//span[contains(@itemprop, 'address')]")[0].text)
+                except:
+                    store.append('NA')
+                try:
+                    store.append(driver.find_element(By.XPATH,'//*[@id="Overview"]/div[1]/h1').text)
+                except:
+                    store.append('NA')
+                store.append("Found")
+                store.append('1')
+                store.append(i)
+                data.append(store)
+                f=1
+        if f==0:
+            store=["Not Found" for i in range(3)]
+            store.append("Found")
+            store.append("0")
+            store.append(i)
+            data.append(store)
 
-            for i in link:
-                url = i.get_attribute('href')
-                if 'maps' in url:
-                    print(url)
-                    urls.append(url)
-                    name.append(i)
-        else:
-            urls.append('NA')
-            name.append(i)
+# show the data 
+print(data)
